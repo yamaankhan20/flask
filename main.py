8651e47ab3ea1bd7b7e1df9e32d3d86020683b08
yamaan <khanyamaan2@gmail.com>
2024-02-05 02:09:16 +0500

    first commit
    Signed-off-by: The Crypto Tribe <am@MacBook-Pro.local>
    Signed-off-by: yamaan <khanyamaan2@gmail.com>

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from datetime import datetime
 import json
 from flask_mail import Mail
 
+
+
 with open('config.json', 'r') as c:
     params = json.load(c)["params"]
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 # app.config.update(
 #     MAIL_SERVER = "smtp.gmail.com",
@@ -40,9 +42,25 @@ class Contact_form(db.Model):
     date = db.Column(db.String(30), nullable=False)
 
 
+class All_posts(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    post_name = db.Column(db.String(50), nullable=False)
+    post_content = db.Column(db.String(130), nullable=False)
+    date = db.Column(db.String(30), nullable=False)
+    slug = db.Column(db.String(25), nullable=False)
+    img_file = db.Column(db.String(15), nullable=False)
+
 @app.route("/")
 def home():
-    return render_template('index.html', param=params, year = year)
+    post_data = All_posts.query.filter_by().all()
+
+    formate_post=[]
+    for my_post in post_data:
+        formatted_date = my_post.date.strftime("%B %d, %Y")
+        formate_post.append((my_post, formatted_date))
+
+    return render_template('index.html', param=params, year = year, post = formate_post[0:2])   #formate_post[0:2] is for pagination meane post per pageee
 
 
 @app.route('/about')
@@ -75,6 +93,16 @@ def contact_us():
     return render_template('contact.html', param=params, year = year)
 
 @app.route('/post/<string:post_slug>', methods = ['GET'])
-def post_single():
-    return render_template('single-post.html')
-app.run(debug=True)
+def post_single(post_slug):
+
+    post = All_posts.query.filter_by(slug = post_slug).first()
+    date = post.date.strftime("%B %d, %Y")
+    return render_template('single-post.html', post= post, param=params, year = year, post_date= date)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html',param=params, year = year), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
